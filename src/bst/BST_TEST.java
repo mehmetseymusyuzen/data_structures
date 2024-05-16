e6562a838a76b58ddb0e4c56fd435849439f0179
Mehmet Seymus Yuzen <mehmtt.21x@gmail.com>
2024-05-16 23:17:39 +0200

    bst --> Created BST_TEST.java

---
+++ src/bst/BST_TEST.java
@@ -0,0 +1,20 @@
+package bst;
+
+public class BST_TEST {
+    public static void main(String[] args) {
+        BST bst = new BST();
+
+        bst.add(30);
+        bst.add(20);
+        bst.add(15);
+        bst.add(40);
+        bst.add(50);
+        bst.add(45);
+        bst.add(38);
+        bst.add(12);
+        bst.add(18);
+
+        bst.print();
+
+    }
+}
